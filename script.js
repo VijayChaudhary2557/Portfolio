fbcd4818d253c2e20faea9cc4f72621db5eabb9d
VijayChaudhary2557 <155866301+VijayChaudhary2557@users.noreply.github.com>
2025-05-20 22:27:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
   AOS.init();
   
-  document.addEventListener("DOMContentLoaded", function () {
-    let typed = new Typed(".typed-text", {
-      strings: ["Fullstack Developer", "Mern Stack Developer", "PHP Laravel Developer"],
-      typeSpeed: 50,
-      backSpeed: 50,
-      backDelay: 1000,
-      loop: true 
-    });
-
-    let d = new Date();
-    let year = d.getFullYear()
-    year = "© "+ year + " Copyright:";
-    document.querySelector('.year').innerHTML = year;
+  let typed = new Typed(".typed-text", {
+    strings: ["Fullstack Developer", "Mern Stack Developer", "PHP Laravel Developer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true 
   });
+  let d = new Date();
+  let year = d.getFullYear()
+  year = "© "+ year + " Copyright:";
+  document.querySelector('.year').innerHTML = year;
+});
 
 
 function myFun(){
